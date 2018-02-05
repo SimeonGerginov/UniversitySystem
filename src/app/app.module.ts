e8f6910432b469296a51038d4a4c825a40b5b245
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-02-05 17:45:06 +0200

    Removed several components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {CommonModule} from '@angular/common';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { appRouting } from './app.routes';
-
 
 @NgModule({
+  imports: [
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    RouterModule,
-    appRouting
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  username: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  roles: [string];
-  profilePictureUrl: string;
-}
