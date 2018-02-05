f18ca2575889d6879a804bda27ae27d12c03e6f5
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-02-05 18:31:49 +0200

    Added home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  pageTitle = 'University System';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   declarations: [
     AppComponent
