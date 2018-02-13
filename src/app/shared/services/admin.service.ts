5effd6f1c35011281384ea4a59f6f24043ef9023
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-02-13 11:34:05 +0200

    Extracted getHeaders functionality as a method

--- src/app/shared/services/admin.service.ts
+++ src/app/shared/services/admin.service.ts
@@ -9,6 +9,7 @@ import { User } from '../models/user.model';
 const DOMAIN_URL = 'http://localhost:3000/api/admin';
 const ALL_MODERATORS_URL = DOMAIN_URL + '/moderators';
 const CREATE_MODERATOR_URL = DOMAIN_URL + '/moderators';
+
 @Injectable()
 export class AdminService {
 
@@ -19,57 +20,44 @@ export class AdminService {
     return this.userStorage.isUserAdmin();
   }
 
-  getAllModerators(): Observable<Response> {
+  getHeaders(): Object {
     const token = this.userStorage.getLoggedUserToken();
     const headers = {
       token,
       'Content-Type': 'application/json'
     };
+
+    return headers;
+  }
+
+  getAllModerators(): Observable<Response> {
+    const headers = this.getHeaders();
 
     return this.httpRequester.get(ALL_MODERATORS_URL, headers);
   }
 
   createModerator(moderator: any): Observable<Response> {
-    const token = this.userStorage.getLoggedUserToken();
-    const headers = {
-      token,
-      'Content-Type': 'application/json'
-    };
+    const headers = this.getHeaders();
 
     return this.httpRequester.post(CREATE_MODERATOR_URL, moderator, headers);
   }
 
   getModerator(id: string): Observable<Response> {
-    const token = this.userStorage.getLoggedUserToken();
-    const headers = {
-      token,
-      'Content-Type': 'application/json'
-    };
-
+    const headers = this.getHeaders();
     const GET_MODERATOR_URL = DOMAIN_URL + `/moderators/${id}`;
 
     return this.httpRequester.get(GET_MODERATOR_URL, headers);
   }
 
   updateModerator(id: string, moderator: User): Observable<Response> {
-    const token = this.userStorage.getLoggedUserToken();
-    const headers = {
-      token,
-      'Content-Type': 'application/json'
-    };
-
+    const headers = this.getHeaders();
     const UPDATE_MODERATOR_URL = DOMAIN_URL + `/moderators/${id}`;
 
     return this.httpRequester.put(UPDATE_MODERATOR_URL, moderator, headers);
   }
 
   deleteModerator(id: string): Observable<Response> {
-    const token = this.userStorage.getLoggedUserToken();
-    const headers = {
-      token,
-      'Content-Type': 'application/json'
-    };
-
+    const headers = this.getHeaders();
     const DELETE_MODERATOR_URL = DOMAIN_URL + `/moderators/${id}`;
 
     return this.httpRequester.delete(DELETE_MODERATOR_URL, headers);
